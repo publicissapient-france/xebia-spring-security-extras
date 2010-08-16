379235acf36b07a9517a9f329ddfeb6178b4c7e9
Cyrille Le Clerc <cleclerc@xebia.fr>
2010-08-16 22:02:50 +0000

    fix typo

--- src/main/java/fr/xebia/audit/AuditAspect.java
+++ src/main/java/fr/xebia/audit/AuditAspect.java
@@ -180,7 +180,7 @@ public class AuditAspect {
             msg.append(" ").append(evaluatedMessage);
 
             if (throwned != null) {
-                msg.append(" thrown");
+                msg.append(" threw");
                 appendThrowableCauses(throwned, ", ", msg);
             }
             msg.append(" by ");
